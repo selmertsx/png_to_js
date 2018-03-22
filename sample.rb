cd1a5f19e3df20f3feab2be2f364f7cea264551a
Morioka Shuhei <selmertsx@gmail.com>
2018-03-22 23:00:10 +0900

    this is sample code

---
+++ sample.rb
@@ -0,0 +1,24 @@
+# coding: UTF-8
+require "zlib"
+
+width, height = 100, 1
+depth, color_type = 8, 2
+
+# グラデーションのベタデータ
+line = [0, ("a"*99).each_codepoint.to_a]
+raw_data = [line] * height
+
+# チャンクのバイト列生成関数
+def chunk(type, data)
+  [data.bytesize, type, data, Zlib.crc32(type + data)].pack("NA4A*N")
+end
+
+# ファイルシグニチャ
+print "\x89PNG\r\n\x1a\n"
+# ヘッダ
+print chunk("IHDR", [width, height, 8, 2, 0, 0, 0].pack("NNCCCCC"))
+# 画像データ
+img_data = raw_data.map {|line| ([0] + line.flatten).pack("C*") }.join
+print chunk("IDAT", Zlib::Deflate.deflate(img_data))
+# 終端
+print chunk("IEND", "")
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-import { Canvas } from 'canvas';
-
-
