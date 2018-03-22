f4cb0afe3139abf4ee696ff751f4b3126628101f
Morioka Shuhei <selmertsx@gmail.com>
2018-03-23 00:12:53 +0900

    create hoge.png

--- sample.rb
+++ sample.rb
@@ -1,21 +1,27 @@
 # coding: UTF-8
 require "zlib"
-require "pry"
 
+width, height = 100, 20
 depth, color_type = 8, 2
-width = 100
+
 # グラデーションのベタデータ
-line = ("a".."z").to_a.sample(width).map(&:ord)
+line = (0...width).map {|x| [97, 0, 0] }
+raw_data = [line] * height
+
 # チャンクのバイト列生成関数
 def chunk(type, data)
   [data.bytesize, type, data, Zlib.crc32(type + data)].pack("NA4A*N")
 end
+
 # ファイルシグニチャ
 print "\x89PNG\r\n\x1a\n"
+
 # ヘッダ
-print chunk("IHDR", [width, 1, 8, 2, 0, 0, 0].pack("NNCCCCC"))
+print chunk("IHDR", [width, height, 8, 2, 0, 0, 0].pack("NNCCCCC"))
+
 # 画像データ
-img_data = ([0] + line.flatten).pack("C*")
+img_data = raw_data.map {|line| ([0] + line.flatten).pack("C*") }.join
 print chunk("IDAT", Zlib::Deflate.deflate(img_data))
+
 # 終端
 print chunk("IEND", "")
