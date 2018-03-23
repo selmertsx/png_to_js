510209fe9a251b7112223153e64e71f0a301fbd0
Morioka Shuhei <selmertsx@gmail.com>
2018-03-23 10:05:30 +0900

    create images

--- ruby_scripts/js_to_png.rb
+++ ruby_scripts/js_to_png.rb
@@ -13,15 +13,10 @@ def chunk(type, data)
   [data.bytesize, type, data, Zlib.crc32(type + data)].pack("NA4A*N")
 end
 
-# ファイルシグニチャ
-print "\x89PNG\r\n\x1a\n"
-
-# ヘッダ
-print chunk("IHDR", [width, height, 8, 2, 0, 0, 0].pack("NNCCCCC"))
-
-# 画像データ
-img_data = raw_data.map {|line| ([0] + line.flatten).pack("C*") }.join
-print chunk("IDAT", Zlib::Deflate.deflate(img_data))
-
-# 終端
-print chunk("IEND", "")
+File.open("images/sample.png", "w", encoding: Encoding::ASCII_8BIT ) do |f|
+  f << "\x89PNG\r\n\x1a\n"
+  f << chunk("IHDR", [width, height, 8, 2, 0, 0, 0].pack("NNCCCCC"))
+  img_data = raw_data.map {|line| ([0] + line.flatten).pack("C*") }.join
+  f << chunk("IDAT", Zlib::Deflate.deflate(img_data))
+  f << chunk("IEND", "")
+end
