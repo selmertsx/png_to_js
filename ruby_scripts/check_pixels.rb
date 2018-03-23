b31a7de8de05bf1578772585c1901404df7e6dba
Morioka Shuhei <selmertsx@gmail.com>
2018-03-23 16:41:55 +0900

    remove unnecessary js

--- ruby_scripts/check_pixels.rb
+++ ruby_scripts/check_pixels.rb
@@ -3,7 +3,7 @@ require 'pry'
 include Magick
 
 decompressed_image = ImageList.new("images/decompress.png")
-image = ImageList.new("images/simple.png")
+image = ImageList.new("images/sample.png")
 
 puts decompressed_image.export_pixels_to_str
 puts image.export_pixels_to_str
