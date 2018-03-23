0d2b13a4876c883b957d963c8f9ff3a2ff059d22
Morioka Shuhei <selmertsx@gmail.com>
2018-03-23 19:55:21 +0900

    scan hogehoge

--- ruby_scripts/check_pixels.rb
+++ ruby_scripts/check_pixels.rb
@@ -5,5 +5,5 @@ include Magick
 decompressed_image = ImageList.new("images/decompress.png")
 image = ImageList.new("images/sample.png")
 
-puts decompressed_image.export_pixels_to_str
-puts image.export_pixels_to_str
+puts decompressed_image.export_pixels_to_str.scan(/.{1,3}/).map(&:chr).join
+puts image.export_pixels_to_str.scan(/.{1,3}/).map(&:chr).join
--- ruby_scripts/js_to_png.rb
+++ ruby_scripts/js_to_png.rb
@@ -8,10 +8,12 @@ depth, color_type = 8, 2
 
 # グラデーションのベタデータ
 raw_data = [code.each_codepoint.to_a.map {|x| [x, x, x]}]
+
 # チャンクのバイト列生成関数
 def chunk(type, data)
   [data.bytesize, type, data, Zlib.crc32(type + data)].pack("NA4A*N")
 end
+
 File.open("images/sample.png", "w", encoding: Encoding::ASCII_8BIT ) do |f|
   f << "\x89PNG\r\n\x1a\n"
   f << chunk("IHDR", [width, 1, 8, 2, 0, 0, 0].pack("NNCCCCC"))
