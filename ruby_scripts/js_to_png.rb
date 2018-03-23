d39aebe9f65fad533ff3b74b2c3dae479d87168a
Morioka Shuhei <selmertsx@gmail.com>
2018-03-23 17:00:45 +0900

    import jquery

--- ruby_scripts/js_to_png.rb
+++ ruby_scripts/js_to_png.rb
@@ -1,20 +1,20 @@
 require "zlib"
+require "pry"
 
-width, height = 100, 1
+jsfile = File.open("jquery_min.js", "r")
+code = jsfile.read
+width = code.length
 depth, color_type = 8, 2
 
 # グラデーションのベタデータ
-line = (0...width).map {|x| [97, 97, 97] }
-raw_data = [line] * height
-
+raw_data = [code.each_codepoint.to_a.map {|x| [x, x, x]}]
 # チャンクのバイト列生成関数
 def chunk(type, data)
   [data.bytesize, type, data, Zlib.crc32(type + data)].pack("NA4A*N")
 end
-
 File.open("images/sample.png", "w", encoding: Encoding::ASCII_8BIT ) do |f|
   f << "\x89PNG\r\n\x1a\n"
-  f << chunk("IHDR", [width, height, 8, 2, 0, 0, 0].pack("NNCCCCC"))
+  f << chunk("IHDR", [width, 1, 8, 2, 0, 0, 0].pack("NNCCCCC"))
   img_data = raw_data.map {|line| ([0] + line.flatten).pack("C*") }.join
   f << chunk("IDAT", Zlib::Deflate.deflate(img_data))
   f << chunk("IEND", "")
