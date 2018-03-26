984d1d867e6353d7295f082e19436a0ad41fa7c1
Morioka Shuhei <selmertsx@gmail.com>
2018-03-26 13:37:14 +0900

    大きなサイズのjsコードを圧縮できるようにした

--- ruby_scripts/js_to_png.rb
+++ ruby_scripts/js_to_png.rb
@@ -1,13 +1,20 @@
 require "zlib"
 require "pry"
 
-jsfile = File.open("sample.js", "r")
+jsfile = File.open("jquery_min.js", "r")
 code = jsfile.read
-width = code.length
+
+# cwebpの制限
+MAXIMUM_PIXEL_SIZE = 16383.0
+width = MAXIMUM_PIXEL_SIZE
+height = (code.length / MAXIMUM_PIXEL_SIZE).ceil
 depth, color_type = 8, 2
 
-# グラデーションのベタデータ
-raw_data = [code.each_codepoint.to_a.map {|x| [x, x, x]}]
+array_code = code.scan(/.{1,16383}/)
+
+raw_data = array_code.map do |data|
+  data.rjust(width, "/").each_codepoint.to_a.map {|x| [x, x, x]}
+end
 
 # チャンクのバイト列生成関数
 def chunk(type, data)
@@ -16,7 +23,7 @@ end
 
 File.open("images/sample.png", "w", encoding: Encoding::ASCII_8BIT ) do |f|
   f << "\x89PNG\r\n\x1a\n"
-  f << chunk("IHDR", [width, 1, 8, 2, 0, 0, 0].pack("NNCCCCC"))
+  f << chunk("IHDR", [width, height, 8, 2, 0, 0, 0].pack("NNCCCCC"))
   img_data = raw_data.map {|line| ([0] + line.flatten).pack("C*") }.join
   f << chunk("IDAT", Zlib::Deflate.deflate(img_data))
   f << chunk("IEND", "")
