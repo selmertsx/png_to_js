b9383d96528bae93460e15dd431da9fed352eee3
Morioka Shuhei <selmertsx@gmail.com>
2018-03-26 14:50:54 +0900

    output js from png

--- ruby_scripts/check_pixels.rb
+++ ruby_scripts/check_pixels.rb
@@ -5,7 +5,10 @@ include Magick
 decompressed_image = ImageList.new("images/decompress.png")
 image = ImageList.new("images/sample.png")
 
-puts "deco:=============="
-puts decompressed_image.export_pixels_to_str.scan(/.{1,3}/).map(&:chr).join.gsub("\s{1,2}","")
-puts "image:=============="
-puts image.export_pixels_to_str.scan(/.{1,3}/).map(&:chr).join.gsub("\s{1,2}","")
+File.open("script_from_pngs/from_webp.js", "w") do |f|
+  f.write(decompressed_image.export_pixels_to_str.scan(/.{1,3}/).map(&:chr).join.gsub("\s{1,2}",""))
+end
+
+File.open("script_from_pngs/png.js", "w") do |f|
+  f.write(image.export_pixels_to_str.scan(/.{1,3}/).map(&:chr).join.gsub("\s{1,2}",""))
+end
